63f5a28ed1d297ee489188a7991fdf8ebe7bd4bf
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-01 18:56:44 +0000

    refactored and lint spinner2

--- spinner2.js
+++ spinner2.js
@@ -1,40 +1,18 @@
-const spinners = ['\r/ ', '\r- ', '\r\\', '\r| ']
+const spinners = ['\r/ ', '\r- ', '\r\\', '\r| ', '\r/ ', '\r- ', '\r\\', '\r| '];
 const spinIt = () => {
-let seconds = 1000;
-for (let element of spinners) {
-  setTimeout(() => {
-    process.stdout.write(element)
-  }, seconds)
-    seconds += 200;
-  } setTimeout (()=> {
-    process.stdout.write('\n')}, seconds + 200*(spinners.length-1)
-    )
+  let seconds = 1000;
+  for (let element of spinners) {
+    setTimeout(() => {
+      process.stdout.write(element);
+    }, seconds);
+    seconds += 100;
+  } setTimeout(() => {
+    process.stdout.write('\n');
+  }, seconds + 50 * (spinners.length - 1)
+  );
 };
 spinIt(spinners);
 
 
 
-// setTimeout(() => {
-//   process.stdout.write('\r/   ');
-// }, 300);
-
-// setTimeout(() => {
-//   process.stdout.write('\r-   ');
-// }, 500);
-
-// setTimeout(() => {
-//   // Need to escape the backslash since it's a special character.
-//   process.stdout.write('\r\\   '); 
-// }, 700);
-
-
-// setTimeout(() => {
-//   // Need to escape the backslash since it's a special character.
-//   process.stdout.write('\r|  '); 
-// }, 900);
-
-// setTimeout(() => {
-//   // Need to escape the backslash since it's a special character.
-//   process.stdout.write('\r\n'); 
-// }, 1200);
 
